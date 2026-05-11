87b7fa6bea4127f316b2483274e77ae513c49cb1
PauPalencia <ppalencia@alum.joviat.cat>
2026-05-11 13:13:58 +0200

    asd

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'PUT_YOUR_API_KEY',
-  authDomain: 'PUT_YOUR_AUTH_DOMAIN',
-  projectId: 'PUT_YOUR_PROJECT_ID',
-  storageBucket: 'PUT_YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'PUT_YOUR_MESSAGING_SENDER_ID',
-  appId: 'PUT_YOUR_APP_ID'
+  apiKey: "AIzaSyAb4GE2zzRgMKIABTVE9qxe4QXm8YFc_ag",
+  authDomain: "videoplayerapp-joviat.firebaseapp.com",
+  projectId: "videoplayerapp-joviat",
+  storageBucket: "videoplayerapp-joviat.firebasestorage.app",
+  messagingSenderId: "808740487021",
+  appId: "1:808740487021:web:da451a171790d4b80ce464",
+  measurementId: "G-2PT29E351S"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 export const db = getFirestore(app);
